b4a9b2e36dfa8ec81f829a29a65cf0ced65eb2f1
xsw <ch_j19@163.com>
2018-06-14 19:08:12 +0800

    test new file

---
+++ TestDemo/TestDemo/testgit.h
@@ -0,0 +1,13 @@
+//
+//  testgit.h
+//  TestDemo
+//
+//  Created by 徐守卫 on 2018/6/14.
+//  Copyright © 2018年 徐守卫. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface testgit : UIView
+
+@end
---
+++ TestDemo/TestDemo/testgit.m
@@ -0,0 +1,21 @@
+//
+//  testgit.m
+//  TestDemo
+//
+//  Created by 徐守卫 on 2018/6/14.
+//  Copyright © 2018年 徐守卫. All rights reserved.
+//
+
+#import "testgit.h"
+
+@implementation testgit
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
